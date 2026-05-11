8d35fee32227fb65f055fe15f81322f76ed7b56e
wwenrr <qscvdefb@gmail.com>
2026-05-11 10:09:05 +0700

    fix: pp init at 0 + hide bottom nav in combat + cache bust v3

--- src/store.js
+++ src/store.js
@@ -801,7 +801,7 @@ const useGameStore = create((set, get) => ({
       ...combatData,
       myHP: 100,
       myStatStages: { atk: 0, def: 0, spd: 0 },
-      myPP: { cuaDam: 20, mo: 25, daBay: 15, khangCu: 10, phongThu: 15 },
+      myPP: { cuaDam: 0, mo: 0, daBay: 0, khangCu: 0, phongThu: 0 },
       combatState: 'player_turn',
     };
     
